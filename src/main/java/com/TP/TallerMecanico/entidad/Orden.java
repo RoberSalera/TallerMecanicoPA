6f63d47a0c918a2522b2c524b5ff4a127a781487
RoberSalera <robertosalera15@gmail.com>
2023-12-10 15:14:45 -0300

    Primer Commit

---
+++ src/main/java/com/TP/TallerMecanico/entidad/Orden.java
@@ -0,0 +1,76 @@
+package com.TP.TallerMecanico.entidad;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="orden")
+public class Orden implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOrden;
+
+    private LocalDate fechaRegistro;
+
+    private LocalDate fechaDocumento;
+
+    @NotEmpty(message = "El kilometraje no debe estar vacio")
+    @Pattern(regexp = "^[0-9]{1,8}$", message = "Debe ser un número de 1 a 8 dígitos.")
+    private String kilometros;
+
+    @OneToMany(mappedBy = "orden")
+    private List<DetalleOrden> detallesOrden;
+
+    @OneToMany(mappedBy = "orden")
+    private List<Imagen> imagenesOrden;
+
+    @ManyToOne
+    @JoinColumn(name = "vehiculo_id")
+    private Vehiculo vehiculo;
+
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
+    private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name = "estado_id")
+    private Estado estadoActual;
+
+    @Transient
+    private String modo;
+    private Boolean estado = true;
+
+    public List<DetalleOrden> getDetallesOrden() {
+        return detallesOrden;
+    }
+
+    public List<DetalleOrden> getDetallesOrdenesHTML(){
+        if (detallesOrden.isEmpty()){
+            return null;
+        } else {
+            return detallesOrden;
+        }
+    }
+
+    public int calcularTotal() {
+        int total = 0;
+        for (DetalleOrden detalleOrden : detallesOrden) {
+            if (detalleOrden.getEstado()) {
+                total += detalleOrden.getSubtotal();
+            }
+        }
+        return total;
+    }
+}
---
+++ src/main/java/com/TP/TallerMecanico/interfaz/IClienteDao.java
@@ -0,0 +1,45 @@
+package com.TP.TallerMecanico.interfaz;
+import com.TP.TallerMecanico.entidad.Cliente;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+
+    boolean existsByIdClienteAndEstadoTrue(Long id);
+
+    @Modifying
+    @Query("UPDATE Cliente m SET m.estado = false WHERE m.idCliente = :idCliente") //Query para el Soft Delete
+    void marcarComoEliminado(@Param("idCliente") Long idCliente);
+
+    List<Cliente> findByEstadoTrue();
+    List<Cliente> findByEstadoFalse();
+
+    @Modifying
+    @Query("UPDATE Cliente m SET m.estado = true WHERE m.idCliente = :idCliente")
+    void marcarComoActivo(@Param("idCliente") Long idCliente);
+
+    Cliente findByDni(String dni);
+
+    Cliente findByIdCliente(Long idCliente);
+
+    Cliente findByDniAndEstadoTrue(String dni);
+
+
+    @Query("SELECT c FROM Cliente c WHERE c.nombre LIKE :nombre% AND c.estado = true")
+    List<Cliente> findByNombreStartingWithAndEstadoTrue(@Param("nombre") String nombre);
+
+
+    @Query("SELECT c FROM Cliente c WHERE c.dni LIKE :dni% AND c.estado = true")
+    List<Cliente> findByDniStartingWithAndEstadoTrue(@Param("dni") String dni);
+
+    @Query("SELECT c FROM Cliente c WHERE c.dni LIKE :dni% AND c.estado = false")
+    List<Cliente> findByDniStartingWithAndEstadoFalse(@Param("dni") String dni);
+
+
+    //List<Cliente> findByNombre(String nombre);
+
+}
